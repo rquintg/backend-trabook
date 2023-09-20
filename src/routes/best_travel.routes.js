5a41dec7885320be25fa3310d48eeaf4c9a8c760
rquintg <ricardoqg41@icloud.com>
2023-09-20 17:41:14 -0500

    :green_heart: Se implementa el Cors y se actualiza routes

---
+++ src/routes/best_travel.routes.js
@@ -0,0 +1,16 @@
+import {Router} from "express";
+import {getBest_travelById, createBestTravel, getBest_travel, updateBestTravel, deleteBestTravel } from "../controllers/best_travel.controller.js";
+
+const router = Router();
+
+router.get('/best', getBest_travel);
+
+router.get('/best/:id', getBest_travelById);
+
+router.post('/best', createBestTravel);
+
+router.patch('/best/:id', updateBestTravel);
+
+router.delete('/best/:id', deleteBestTravel);
+
+export default router;
